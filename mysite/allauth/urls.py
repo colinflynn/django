6b01788f0b30ac9a4461fb5e91c7fdd2cffd0d8e
Flynn <212569024@logon.ds.ge.com>
2018-09-02 20:50:14 -0400

    initial commit

---
+++ mysite/allauth/urls.py
@@ -0,0 +1,24 @@
+from importlib import import_module
+
+from django.conf.urls import include, url
+
+from allauth.socialaccount import providers
+
+from . import app_settings
+
+
+urlpatterns = [url(r'^', include('allauth.account.urls'))]
+
+if app_settings.SOCIALACCOUNT_ENABLED:
+    urlpatterns += [url(r'^social/', include('allauth.socialaccount.urls'))]
+
+for provider in providers.registry.get_list():
+    print provider
+    try:
+        prov_mod = import_module(provider.get_package() + '.urls')
+    except ImportError as e:
+        print e
+        continue
+    prov_urlpatterns = getattr(prov_mod, 'urlpatterns', None)
+    if prov_urlpatterns:
+        urlpatterns += prov_urlpatterns
